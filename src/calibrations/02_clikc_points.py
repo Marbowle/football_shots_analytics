a854d7517d39403e5b1fdcdb7e446a9fda07bad2
Marta Domagała <mahomegol@gmail.com>
2025-11-05 11:53:24 +0100

    Create files for calibrations left side

--- src/calibrations/02_clikc_points.py
+++ src/calibrations/02_clikc_points.py
@@ -4,25 +4,28 @@ import json
 
 points = []
 
+img_path = "D:/football_shots_analytics/data/calibrations/h_left.png"
+img = cv2.imread(img_path)
+
+img_dis = img.copy()
+
 def click_points(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
         if len(points) < 10:
             points.append((x, y))
             print(f"Clicked points: {x}, {y}")
-            img_display = img.copy()
             font = cv2.FONT_HERSHEY_SIMPLEX
             label = str(len(points))
-            cv2.putText(img_display, label, (x, y), font, 1, (255, 255, 255), 2)
-            cv2.circle(img_display, (x, y), 5, (0, 0, 255), 2)
+            cv2.putText(img_dis, label, (x, y), font, 1, (255, 255, 255), 2)
+            cv2.circle(img_dis, (x, y), 5, (0, 0, 255), 2)
+            cv2.imshow("Image", img_dis)
+            cv2.waitKey(1)
         elif len(points) == 10:
             print("Kliknięto 10 punktów!")
             cv2.destroyAllWindows()
 
-img_path = "/data/calibrations/h_left.png"
 
-img = cv2.imread(img_path)
-img_display = img.copy()
-cv2.imshow("Process of clicking points", img_display)
+cv2.imshow("Process of clicking points", img_dis)
 cv2.setMouseCallback("Process of clicking points", click_points)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
